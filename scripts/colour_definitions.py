b3b7eae254240f7c3a92ba6da84a5dfbccbb123f
TCLamnidis <thisseass@gmail.com>
2020-11-19 14:26:53 +0100

    Added colour definitions for snp_sets

--- scripts/colour_definitions.py
+++ scripts/colour_definitions.py
@@ -1,10 +1,23 @@
 ## Colour definitions for populations
-population_colours={"Pop0":"black",
-      "Pop1":"blue",
-      "Pop2":"red",
-      "Pop3":"gold",
-      "Pop4":"purple",
-      "Pop5":"green",
-      "Pop6":"magenta",
-      "Pop7":"orange",
-      "Pop8":"grey"}
+population_colours={
+  "Pop0":"black",
+  "Pop1":"blue",
+  "Pop2":"red",
+  "Pop3":"gold",
+  "Pop4":"purple",
+  "Pop5":"green",
+  "Pop6":"magenta",
+  "Pop7":"orange",
+  "Pop8":"grey"
+}
+
+## Colour definitions for snp_sets (for KNN)
+snp_set_colours={
+  "all":"blue",
+  "common":"purple",
+  "1240k":"magenta",
+  "rare_ac2":"gray60",
+  "rare_ac3":"gray70",
+  "rare_ac4":"gray80",
+  "rare_ac5":"gray90"
+}
