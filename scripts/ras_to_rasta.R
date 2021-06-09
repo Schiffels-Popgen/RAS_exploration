66bdc81771b4809eaa38feae68e55c2b568e7046
Thiseas Christos Lamnidis <thisseass@gmail.com>
2021-06-09 10:53:10 +0200

    Infer script path for sourcing Defs.R

--- scripts/ras_to_rasta.R
+++ scripts/ras_to_rasta.R
@@ -10,8 +10,18 @@
 library("tidyr") ## Cannot find a way to use %>% with a package specific call
 # library("purrr", quietly = TRUE)
 
+## Function to get the path of this script. Needed to source Defs.R correctly.
+getScriptPath <- function(){
+    cmd.args <- commandArgs()
+    m <- regexpr("(?<=^--file=).+", cmd.args, perl=TRUE)
+    script.dir <- dirname(regmatches(cmd.args, m))
+    if(length(script.dir) == 0) stop("can't determine script dir: please call the script with Rscript")
+    if(length(script.dir) > 1) stop("can't determine script dir: more than one '--file' argument detected")
+    return(script.dir)
+}
+
 ## Source function definitions
-source("Defs.R")
+source(paste(getScriptPath(),"Defs.R", sep="/"))
 
 ## Read command line arguments
 args = commandArgs(trailingOnly=TRUE)
