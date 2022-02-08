c5bbd5b4f8a4ab7da2beba40fe7536a45fa6bd68
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2022-02-08 14:29:48 +0100

    first run of RAS on HGDP finished

--- analyses/HGDP-exploration/run_ras.sh
+++ analyses/HGDP-exploration/run_ras.sh
@@ -4,5 +4,5 @@ DATA_DIR=/mnt/archgen/MICROSCOPE/rarevar_sim_study/real_data
 POS25_ANCIENT=$DATA_DIR/ancient/ancientBritish/hgdp/poseidon2.5
 POS25_MODERN=$DATA_DIR/modern/HGDP/plink/poseidon/all_vars_2.5
 
-qsub -b y -cwd xerxes ras -d $POS25_ANCIENT -d $POS25_MODERN --popConfigFile pop_config.yml -f AncientBritish_HGDP_ras.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS25_ANCIENT -d $POS25_MODERN -j 100000 --popConfigFile pop_config.yml -f AncientBritish_HGDP_ras.table.txt
 
