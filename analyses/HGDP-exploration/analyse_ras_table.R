865bdd8305a932cedc2ce51e94caffb6f4dd12f0
Stephan Schiffels <stephan_schiffels@mac.com>
2022-02-21 20:28:16 +0100

    continued analysis

--- analyses/HGDP-exploration/analyse_ras_table.R
+++ analyses/HGDP-exploration/analyse_ras_table.R
@@ -1,7 +1,9 @@
 library(magrittr)
+library(ggplot2)
 
-ras_table <- readr::read_delim("analyses/HGDP-exploration/AncientBritish_HGDP_ras.table.txt", 
-                               delim = "\t", escape_double = FALSE, trim_ws = TRUE)
+ras_table <- readr::read_tsv("analyses/HGDP-exploration/AncientBritish_HGDP_ras.table.txt")
 
-ras_table %>% dplyr::filter(Right=="Basque2")
+ras_table %>% dplyr::filter(Cumulative==TRUE, k==5) %>% ggplot() +
+  geom_errorbar(aes(x=Left, y=RAS, ymin=RAS-StdErr, ymax=RAS+StdErr, col=Right)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
