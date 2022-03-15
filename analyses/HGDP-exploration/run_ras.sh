cbefc03adbb6ec71c117a2611e38bdd29243fe49
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2022-03-15 17:11:33 +0100

    added some analyses

--- analyses/HGDP-exploration/run_ras.sh
+++ analyses/HGDP-exploration/run_ras.sh
@@ -4,10 +4,16 @@ DATA_DIR=/mnt/archgen/MICROSCOPE/rarevar_sim_study/real_data
 POS_ANCIENT=$DATA_DIR/ancient/ancientBritish/hgdp/poseidon
 POS_MODERN=$DATA_DIR/modern/HGDP/plink/poseidon/all_vars
 
-qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --popConfigFile pop_config.yml -f AncientBritish_HGDP_ras.table.txt
+# qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --popConfigFile pop_config.yml -f AncientBritish_HGDP_ras.table.txt
+# xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --maxSnps 1000000 --popConfigFile pop_config.yml -f AncientBritish_HGDP_ras.table.txt
+# xerxes csfs -d $POS_ANCIENT -d $POS_MODERN --maxSnps 100000 --popConfigFile pop_config.yml -f AncientBritish_HGDP_csfs.table.txt
 
-# DATA_DIR=/mnt/archgen/MICROSCOPE/rarevar_sim_study/real_data
-# POS_ANCIENT=$DATA_DIR/ancient/ancientBritish/1000g/poseidon
-# POS_MODERN=$DATA_DIR/modern/1000G/plink/poseidon/all_vars
+
+
+DATA_DIR=/mnt/archgen/MICROSCOPE/rarevar_sim_study/real_data
+POS_ANCIENT=$DATA_DIR/ancient/ancientBritish/1000g/poseidon
+POS_MODERN=$DATA_DIR/modern/1000G/plink/poseidon/all_vars
 
 # qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --popConfigFile pop_config_TGP.yml -f AncientBritish_1000G_ras.table.txt
+xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --maxSnps 1000000 --popConfigFile pop_config_TGP.yml -f AncientBritish_1000G_ras.table.txt
+xerxes csfs -d $POS_ANCIENT -d $POS_MODERN --maxSnps 100000 --popConfigFile pop_config_TGP.yml -f AncientBritish_1000G_csfs.table.txt
