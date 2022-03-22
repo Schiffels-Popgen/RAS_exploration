ca2e5e8df28e12ced6bac97f49c4c0424d6cea1e
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2022-03-22 21:00:52 +0100

    new runs for ras on HGDP and 1000G

--- analyses/HGDP-exploration/run_ras.sh
+++ analyses/HGDP-exploration/run_ras.sh
@@ -4,15 +4,50 @@ DATA_DIR=/mnt/archgen/MICROSCOPE/rarevar_sim_study/real_data
 POS_ANCIENT=$DATA_DIR/ancient/ancientBritish/hgdp/poseidon
 POS_MODERN=$DATA_DIR/modern/HGDP/plink/poseidon/all_vars
 
-qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --popConfigFile pop_config.yml -f AncientBritish_HGDP_ras.table.txt
-# xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --maxSnps 1000000 --popConfigFile pop_config.yml -f AncientBritish_HGDP_ras.table.txt
-# xerxes csfs -d $POS_ANCIENT -d $POS_MODERN --maxSnps 100000 --popConfigFile pop_config.yml -f AncientBritish_HGDP_csfs.table.txt
+# Rightpops ~200 individuals
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.02 --popConfigFile pop_config.yml \
+  -f AncientBritish_HGDP_ras02.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.05 --popConfigFile pop_config.yml \
+  -f AncientBritish_HGDP_ras05.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.1 --popConfigFile pop_config.yml \
+  -f AncientBritish_HGDP_ras10.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.2 --popConfigFile pop_config.yml \
+  -f AncientBritish_HGDP_ras20.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --noMaxFreq --popConfigFile pop_config.yml -f AncientBritish_HGDP_rasAll.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --minFreq 0.05 --maxFreq 0.95 --popConfigFile pop_config.yml \
+  -f AncientBritish_HGDP_rasCommon.table.txt
 
 
 DATA_DIR=/mnt/archgen/MICROSCOPE/rarevar_sim_study/real_data
 POS_ANCIENT=$DATA_DIR/ancient/ancientBritish/1000g/poseidon
 POS_MODERN=$DATA_DIR/modern/1000G/plink/poseidon/all_vars
 
-qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --popConfigFile pop_config_TGP.yml -f AncientBritish_1000G_ras.table.txt
-# xerxes ras -d $POS_ANCIENT -d $POS_MODERN -j 100000 --maxSnps 1000000 --popConfigFile pop_config_TGP.yml -f AncientBritish_1000G_ras.table.txt
-# xerxes csfs -d $POS_ANCIENT -d $POS_MODERN --maxSnps 100000 --popConfigFile pop_config_TGP.yml -f AncientBritish_1000G_csfs.table.txt
+# Rightpops ~800 individuals
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.01 --popConfigFile pop_config_TGP.yml \
+  -f AncientBritish_1000G_ras01.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.02 --popConfigFile pop_config_TGP.yml \
+  -f AncientBritish_1000G_ras02.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.05 --popConfigFile pop_config_TGP.yml \
+  -f AncientBritish_1000G_ras05.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.1 --popConfigFile pop_config_TGP.yml 
+  -f AncientBritish_1000G_ras10.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --maxFreq 0.2 --popConfigFile pop_config_TGP.yml 
+  -f AncientBritish_1000G_ras20.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --noMinFreq --noMaxFreq --popConfigFile pop_config_TGP.yml 
+  -f AncientBritish_1000G_rasAll.table.txt
+qsub -b y -cwd -l h_vmem=16G xerxes ras -d $POS_ANCIENT -d $POS_MODERN \
+  -j 100000 --minFreq 0.05 --maxFreq 0.95 --popConfigFile pop_config_TGP.yml 
+  -f AncientBritish_1000G_rasCommon.table.txt
+
