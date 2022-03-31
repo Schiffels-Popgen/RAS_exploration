b3316ca0f3f81842db3541f09385e9c329c73bfd
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2022-03-31 17:43:18 +0200

    Start consolidating analyses so far into an R notebook

--- analyses/1000G-HGDP-exploration/analyse_ras_table_tvOnly.R
+++ analyses/1000G-HGDP-exploration/analyse_ras_table_tvOnly.R
@@ -1,68 +1,18 @@
 library(magrittr)
 library(ggplot2)
+source("analyses/1000G-HGDP-exploration/load_data.R")
 
-pat <- "AncientBritish_(HGDP|1000G)_ras([0-9A-Za-z]+)_TVonly.table.txt"
-
-popNames <- tibble::tribble(
-  ~Left, ~Group,
-  "<12880A>", "AncientBritish",
-  "<12881A>", "AncientBritish",
-  "<12883A>", "AncientBritish",
-  "<12884A>", "AncientBritish",
-  "<12885A>", "AncientBritish",
-  "<15558A>", "AncientBritish",
-  "<15569A>", "AncientBritish",
-  "<15570A>", "AncientBritish",
-  "<15577A>", "AncientBritish",
-  "<15579A>", "AncientBritish",
-  "<3DT16>",  "AncientBritish",
-  "<3DT26>",  "AncientBritish",
-  "<6DT18>",  "AncientBritish",
-  "<6DT21>",  "AncientBritish",
-  "<6DT22>",  "AncientBritish",
-  "<6DT23>",  "AncientBritish",
-  "<6DT3>",   "AncientBritish",
-  "<M1489>",  "AncientBritish",    
-  "<NO3423>", "AncientBritish",
-  "<NA12889>", "CEU",
-  "<NA12890>", "CEU",
-  "<HG00383>", "FIN",
-  "<HG00384>", "FIN",
-  "<HG01791>", "GBR",
-  "<HG02215>", "GBR",
-  "<HG02238>", "IBS", 
-  "<HG02239>", "IBS",
-  "<NA20828>", "TSI",
-  "<NA20832>", "TSI",
-  "<NA18749>", "CHB",
-  "<NA18757>", "CHB",
-  "<NA19248>", "YRI",
-  "<NA19256>", "YRI",
-  "<NA19257>", "YRI",
-  "<HG02348>", "PEL",
-  "<HG02425>", "PEL"
-)
-
-read_ras_table <- function(full_filename) {
-  fn <- basename(full_filename)
-  m <- stringr::str_match(fn, pat)
-  dataset <- m[2]
-  af <- m[3]
-  readr::read_tsv(full_filename, col_types="ccidd") %>%
-    dplyr::mutate(dataset = dataset, rasAF = af) %>%
-    dplyr::left_join(popNames)
+tgp_scatter_plot <- function(rasAF_, TF_, tvOnly_, mapMasked_) {
+  ras_table %>% dplyr::filter(dataset == "1000G" & TF == TF_ & rasAF == rasAF_ & tvOnly == tvOnly_ & mapMasked == mapMasked_ &
+                                !(Right %in% c("CHB2", "YRI2")) & !(Group %in% c("YRI", "CHB", "PEL"))) %>%
+    dplyr::select(Left, Group, Right, RAS, StdErr) %>%
+    ggplot(aes(x = Left, y = RAS, col = Group)) + geom_point() +
+    geom_errorbar(aes(ymin = RAS - StdErr, ymax = RAS + StdErr)) +
+    facet_wrap(~Right) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 }
 
-ras_table <- list.files("analyses/HGDP-exploration", pattern = pat, full.names=TRUE) %>%
-  purrr::map_dfr(~read_ras_table(.))
-
-ras_table %>% dplyr::filter(dataset == "1000G" & rasAF == "01" & !(Right %in% c("CHB2", "YRI2")) &
-                              !(Group %in% c("YRI", "CHB", "PEL"))) %>%
-  dplyr::select(Left, Group, Right, RAS, StdErr) %>%
-  ggplot(aes(x = Left, y = RAS, col = Group)) + geom_point() +
-  geom_errorbar(aes(ymin = RAS - StdErr, ymax = RAS + StdErr)) +
-  facet_wrap(~Right) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+tgp_scatter_plot("01", FALSE, TRUE, TRUE)
 
 ggsave("sanalyses/HGDP-exploration/scatter_plot_1000G_ras01_tvOnly.pdf", plot1)
 
---
+++ analyses/1000G-HGDP-exploration/load_data.R
@@ -0,0 +1,57 @@
+file_pattern <- "AncientBritish_(HGDP|1000G)_(1240K_){0,1}ras([0-9A-Za-z]+)(_TVonly){0,1}(_mapMasked){0,1}.table.txt"
+
+popNames <- tibble::tribble(
+  ~Left, ~Group,
+  "<12880A>", "AncientBritish",
+  "<12881A>", "AncientBritish",
+  "<12883A>", "AncientBritish",
+  "<12884A>", "AncientBritish",
+  "<12885A>", "AncientBritish",
+  "<15558A>", "AncientBritish",
+  "<15569A>", "AncientBritish",
+  "<15570A>", "AncientBritish",
+  "<15577A>", "AncientBritish",
+  "<15579A>", "AncientBritish",
+  "<3DT16>",  "AncientBritish",
+  "<3DT26>",  "AncientBritish",
+  "<6DT18>",  "AncientBritish",
+  "<6DT21>",  "AncientBritish",
+  "<6DT22>",  "AncientBritish",
+  "<6DT23>",  "AncientBritish",
+  "<6DT3>",   "AncientBritish",
+  "<M1489>",  "AncientBritish",    
+  "<NO3423>", "AncientBritish",
+  "<NA12889>", "CEU",
+  "<NA12890>", "CEU",
+  "<HG00383>", "FIN",
+  "<HG00384>", "FIN",
+  "<HG01791>", "GBR",
+  "<HG02215>", "GBR",
+  "<HG02238>", "IBS", 
+  "<HG02239>", "IBS",
+  "<NA20828>", "TSI",
+  "<NA20832>", "TSI",
+  "<NA18749>", "CHB",
+  "<NA18757>", "CHB",
+  "<NA19248>", "YRI",
+  "<NA19256>", "YRI",
+  "<NA19257>", "YRI",
+  "<HG02348>", "PEL",
+  "<HG02425>", "PEL"
+)
+
+read_ras_table <- function(full_filename) {
+  fn <- basename(full_filename)
+  m <- stringr::str_match(fn, file_pattern)
+  dataset <- m[2]
+  TF <- if(is.na(m[3])) FALSE else TRUE
+  af <- m[4]
+  tvOnly <- if(is.na(m[5])) FALSE else TRUE
+  mapMasked <- if(is.na(m[6])) FALSE else TRUE
+  readr::read_tsv(full_filename, col_types="ccidd") %>%
+    dplyr::mutate(dataset = dataset, TF = TF, rasAF = af, tvOnly = tvOnly, mapMasked = mapMasked) %>%
+    dplyr::left_join(popNames)
+}
+
+ras_table <- list.files(pattern = file_pattern, full.names=TRUE) %>%
+  purrr::map_dfr(~read_ras_table(.))
