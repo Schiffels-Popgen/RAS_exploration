165ce96eba5e96265c39f9c0e3fc2fead3fd946b
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2022-04-07 15:42:36 +0200

    added more runs

--- analyses/1000G-HGDP-exploration/Cluster/run_ras_tv.hs
+++ analyses/1000G-HGDP-exploration/Cluster/run_ras_tv.hs
@@ -18,6 +18,15 @@ pos_modern = data_dir </> "modern/1000G/plink/poseidon/all_vars"
 bed_file :: FilePath
 bed_file = "/mnt/archgen/users/schiffels/hs37m_filt35_99.bed"
 
+repo_dir :: FilePath
+repo_dir = "/mnt/archgen/poseidon/published_data"
+
+pos_ancient_hgdp :: FilePath
+pos_ancient_hgdp = data_dir </> "ancient/ancientBritish/hgdp/poseidon"
+
+pos_modern_hgdp :: FilePath
+pos_modern_hgdp = data_dir </> "modern/HGDP/plink/poseidon/all_vars"
+
 main :: IO ()
 main = do
     let process cmd = do
@@ -25,9 +34,9 @@ main = do
             stdout (inshell cmd empty)
     sh $ do
         -- afStr <- select ["01", "02", "05", "10", "20", "Common", "All"]
-        let afStr = "Common"
-        let mapMasked = True
-        -- mapMasked <- select [False, True]
+        afStr <- select ["02", "05", "10", "20", "Common", "All"]
+        mapMasked <- select [False, True]
+        let dataset = "HGDP"
         let afCond = case afStr of
                 "01"     -> "--noMinFreq    --maxFreq 0.01"
                 "02"     -> "--noMinFreq    --maxFreq 0.02"
@@ -39,8 +48,19 @@ main = do
         let bedOpt = if mapMasked then
                 format ("--bedFile "%fp%" ") bed_file else " "
         let bedStr = if mapMasked then "_mapMasked" else ""
-        liftIO . process $ format ("qsub -V -b y -cwd -l h_vmem=16G xerxes ras -d "%fp%" -d "%fp%
-            " -j 100000 --noTransitions "%s%" --popConfigFile pop_config_TGP.yml "%s%
-            "-f ../Data/AncientBritish_1000G_ras"%s%"_TVonly"%s%".table.txt") pos_ancient pos_modern afCond bedOpt afStr bedStr
+        let outFN =
+                if dataset == "1000G" then
+                        format("AncientBritish_1000G_ras"%s%"_TVonly"%s%".table.txt") afStr bedStr
+                else
+                        format("AncientBritish_HGDP_ras"%s%"_TVonly"%s%".table.txt") afStr bedStr
+        let base_dirs =
+                if dataset == "1000G" then
+                        format ("-d "%fp%" -d "%fp) pos_ancient pos_modern
+                else
+                        format ("-d "%fp%" -d "%fp) pos_ancient_hgdp pos_modern_hgdp
+        let config_file = if dataset == "1000G" then "pop_config_TGP.yml" else "pop_config_HGDP.yml"
+        liftIO . process $ format ("qsub -V -b y -cwd -l h_vmem=16G xerxes ras "%s%
+            " -j 100000 --noTransitions "%s%" --popConfigFile "%s%" "%s%
+            "-f ../Data/"%s) base_dirs afCond config_file bedOpt outFN
 
   
